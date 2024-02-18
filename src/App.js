37c5872e52c34cbfb2858ce7eb4fd0733736984a
mallikharjun <arjun.malli068@gmail.com>
2024-02-18 14:14:56 +0530

    increment counter

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import './App.css';
 
 function App() {
   const  [count, setCount] =useState(0);
+  const increment=()=>{
+    setCount((prev)=>prev + 1)
+  }
   return (
     <div className="App">
       <h1>Tejuuu</h1>
       <h2>{count}</h2>
+      <button onClick={increment}>Increment</button>
     </div>
   );
 }
