fa527ee5d4bd11180329d99af67dcd6057b8a62a
mallikharjun <arjun.malli068@gmail.com>
2024-02-18 14:50:10 +0530

    counter added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import React, { useState }  from 'react'
 // import { useState } from 'react';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   const  [count, setCount] =useState(0);
@@ -13,6 +14,7 @@ function App() {
       <h1>Tejuuu</h1>
       <h2>{count}</h2>
       <button onClick={increment}>Increment</button>
+      <Todo/>
     </div>
   );
 }
